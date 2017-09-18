538b003275fb83b7e79272a7d25e79386196a8f0
Özenç Çelik <ozenc.celik@ceng.deu.edu.tr>
2017-09-18 18:05:06 +0300

    Tab Bar hatası düzeltidi

--- app/src/main/java/com/example/sinem/giraffe/SignUpRegisterActivity.java
+++ app/src/main/java/com/example/sinem/giraffe/SignUpRegisterActivity.java
@@ -2,28 +2,21 @@ package com.example.sinem.giraffe;
 
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.ui.User;
-import com.google.android.gms.internal.zzdlc;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.*;
-import com.google.firebase.auth.UserInfo;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.List;
 
 
 public class SignUpRegisterActivity extends BaseActivity {
@@ -110,22 +103,27 @@ public class SignUpRegisterActivity extends BaseActivity {
     private void createAccount(){
         String email = userEmail.getText().toString();
         String password = userPassword.getText().toString();
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener
-                (this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                Log.d(TAG, "createUserWithEmail:onComplete: "+task.isSuccessful());
-                Intent login= new Intent(SignUpRegisterActivity.this,MainActivity.class);
-                startActivity(login);
 
-                if(!task.isSuccessful()){
-                    Toast.makeText(SignUpRegisterActivity.this,
-                            "Kaydınız yapılamadı... Tekrar deneyiniz!",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:onComplete: "+task.isSuccessful());
+                            Intent login= new Intent(SignUpRegisterActivity.this,MainActivity.class);
+                            startActivity(login);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpRegisterActivity.this,
+                                    "Kaydınız yapılamadı... Tekrar deneyiniz!",Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
     }
-
 
 
 
